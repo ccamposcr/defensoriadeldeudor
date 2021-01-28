7791cf09b524e09b83738f031398091b275fb63c
ccamposcr <ccamposcr@gmail.com>
2021-01-27 21:27:59 -0600

    vue single file components implemented

--- application/views/global/footer.php
+++ application/views/global/footer.php
@@ -1,8 +1,8 @@
 <?php $CI =& get_instance(); ?>
 <script type="text/javascript">
-    var base_url = '<?php echo base_url();?>';
-    var csrf_name = '<?php echo $CI->security->get_csrf_token_name(); ?>';
-    var csrf_hash = '<?php echo $CI->security->get_csrf_hash(); ?>';
+    const base_url = '<?php echo base_url();?>';
+    const csrf_name = '<?php echo $CI->security->get_csrf_token_name(); ?>';
+    const csrf_hash = '<?php echo $CI->security->get_csrf_hash(); ?>';
 </script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>assets/dist/build.js"></script>
---
+++ assets/src/components/Clientlist.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul><li v-bind:key="item.id" v-for="item in users">{{ item.name }}</li></ul>
+</template>
+
+<script>
+export default {
+  name: 'Clientlist',
+  data () {
+    return {
+      users: []
+    }
+  },
+  created: function(){ // Perfect step to retrieve async data
+      this.getUsers();
+  },
+  methods: {
+      getUsers: async function(){
+          const res = await fetch('clientes/getAllClients');
+          const data = await res.json();
+          this.users = data;
+      }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
