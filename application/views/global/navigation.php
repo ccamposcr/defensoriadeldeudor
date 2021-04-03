3cf695ab7c04504c5d8d3be4971536cdacd922ac
ccamposcr <ccamposcr@gmail.com>
2021-04-03 02:02:52 -0600

    appliying global styles

--- application/views/global/navigation.php
+++ application/views/global/navigation.php
@@ -1,20 +1,22 @@
-<ul class="nav">
-  <li class="nav__item" role="presentation"><a class="btn btn btn-secondary" href="<?php echo base_url(); ?>">Inicio</a></li>
-  <li class="nav__item" role="presentation"><a class="btn btn btn-secondary" href="<?php echo base_url(); ?>clientes">Clientes</a></li>
-  <?php
-   if(in_array("7", json_decode($this->session->userdata('roleAccessList'))))
-   {
-      echo '<li class="nav__item" role="presentation" ><a class="btn btn btn-secondary" href="'. base_url() . 'register">Crear Usuarios</a></li>';
-   }
-   if(in_array("9", json_decode($this->session->userdata('roleAccessList'))))
-   {
-      echo '<li class="nav__item" role="presentation"><a class="btn btn btn-secondary" href="'. base_url() . 'clientes?showSystemUsers=true">Editar Usuarios</a></li>';
-   }
-   if(in_array("8", json_decode($this->session->userdata('roleAccessList'))))
-   {
-      echo '<li class="nav__item" role="presentation"><a class="btn btn btn-secondary" href="'. base_url() . 'administracion">Administrar</a></li>';
-   }
-   ?>
-    <li class="nav__item" role="presentation"><a class="btn btn btn-danger" href="<?php echo base_url(); ?>logout">Desconectarse</a></li>
-</ul>
+<div class="navigation">
+   <ul class="nav">
+      <li class="nav__item" role="presentation"><a class="btn btn btn-secondary" href="<?php echo base_url(); ?>">Inicio</a></li>
+      <li class="nav__item" role="presentation"><a class="btn btn btn-secondary" href="<?php echo base_url(); ?>clientes">Clientes</a></li>
+      <?php
+         if(in_array("7", json_decode($this->session->userdata('roleAccessList'))))
+         {
+            echo '<li class="nav__item" role="presentation" ><a class="btn btn btn-secondary" href="'. base_url() . 'register">Crear Usuarios</a></li>';
+         }
+         if(in_array("9", json_decode($this->session->userdata('roleAccessList'))))
+         {
+            echo '<li class="nav__item" role="presentation"><a class="btn btn btn-secondary" href="'. base_url() . 'clientes?showSystemUsers=true">Editar Usuarios</a></li>';
+         }
+         if(in_array("8", json_decode($this->session->userdata('roleAccessList'))))
+         {
+            echo '<li class="nav__item" role="presentation"><a class="btn btn btn-secondary" href="'. base_url() . 'administracion">Administrar</a></li>';
+         }
+         ?>
+         <li class="nav__item" role="presentation"><a class="btn btn btn-danger" href="<?php echo base_url(); ?>logout">Desconectarse</a></li>
+   </ul>
+</div>
 
